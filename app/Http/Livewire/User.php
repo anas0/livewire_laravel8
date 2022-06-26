cf0e8d6f98c58e45e01ece09360ee1d633e262c8
anas0 <ashraful0a@gmail.com>
2022-06-26 14:42:46 +0600

    Livewire With Database

--- app/Http/Livewire/User.php
+++ app/Http/Livewire/User.php
@@ -3,15 +3,22 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\User as Users;
 
 class User extends Component
 {
+    public $users;
+
     public function render()
     {
-        return <<<'blade'
-            <div>
-                <h1>this is the User component</h1>
-            </div>
-        blade;
+        $this->users = Users::all();
+
+        return view('livewire.user');
+        
+        // return <<<'blade'
+        //     <div>
+        //         <h1>this is the User component</h1>
+        //     </div>
+        // blade;
     }
 }
